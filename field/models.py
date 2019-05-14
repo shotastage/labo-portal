c973cb7564640d7ef7d5bfdc53fb4dd3db0d98d4
Shota Shimazu <hornet.live.mf@gmail.com>
2019-05-14 14:21:53 +0900

    add model

--- field/models.py
+++ field/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Content(models.Model):
+  title = models.CharField(max_length = 30)
+  text = models.TextField()
+  content_type = models.CharField(max_length = 30)
+  created_by = models.CharField(max_length = 50)
+  is_deleted = models.BooleanField()
+  is_updated = models.BooleanField()
