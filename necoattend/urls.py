d1989399767c7a75922d8436826c09314a84e6e4
Shota Shimazu <hornet.live.mf@gmail.com>
2019-05-16 17:15:26 +0900

    add url

--- necoattend/urls.py
+++ necoattend/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('api/', include('field.urls')),
     path('kgl/', include('manager.urls')),
     path('kg/', include('enrollment.urls')),
-    path('admin/', admin.site.urls),
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    path('admin/', include(admin.site.urls)),
 ]
