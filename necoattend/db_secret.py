c5e63cb190a2f7174034e8ec59ba3af104e49650
Shota Shimazu <hornet.live.mf@gmail.com>
2019-09-23 17:48:54 +0900

    revise

--- necoattend/db_secret.py
+++ necoattend/db_secret.py
@@ -1,16 +1,24 @@
 import os, sys
-
+import dj_database_url
 
 try:
-    database_setting = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ["NSYS_DB_NAME"],
-            'USER': os.environ["NSYS_DB_USER"],
-            'PASSWORD': os.environ["NSYS_DB_PASS"],
-            'HOST': os.environ["NSYS_DB_HOST"],
-            'PORT': '5432',
+
+    if "postgres://" in os.environ["DATABASE_URL"]:
+        database_setting = {
+            'default': {
+                dj_database_url.config(default=os.environ["DATABASE_URL"])
+            }
         }
-    }
+    else:
+        database_setting = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ["NSYS_DB_NAME"],
+                'USER': os.environ["NSYS_DB_USER"],
+                'PASSWORD': os.environ["NSYS_DB_PASS"],
+                'HOST': os.environ["NSYS_DB_HOST"],
+                'PORT': '5432',
+            }
+        }
 except:
     database_setting = {}
