228a13e6fabfff6e4c01525604cfe41e5bf43651
Shota Shimazu <hornet.live.mf@gmail.com>
2021-05-20 15:19:33 +0900

    revise

--- discordi/views.py
+++ discordi/views.py
@@ -12,24 +12,21 @@ from django.views.decorators.csrf import csrf_exempt
 class APISubmitView(View):
 
     def post(self, request):
-        token = request.META["HTTP_API_TOKEN"]
         body = request.body.decode('utf-8')
 
 
-        if os.environ['ADMIN_TOKEN'] == token:
-            atd = Attendances(
-                login=str(body),
-                mtg_id="7a450d05-1e49-4f45-a6f0-9eec9159fe01",
-                bw_id="",
-                ip_address="",
-                attend=True
-            )
+        atd = Attendances(
+            login=str(body),
+            mtg_id="7a450d05-1e49-4f45-a6f0-9eec9159fe01",
+            bw_id="",
+            ip_address="",
+            attend=True
+        )
 
-            atd.save()
+        atd.save()
 
-            return HttpResponse("Successful created new api doc!", status=201)
-        else:
-            return HttpResponse("Access token is wrong!", status=403)
+        return HttpResponse("Successful created new api doc!", status=201)
+    
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
