99353fac9c0844ef83de130fcef95f9afbce30a1
Shota Shimazu <hornet.live.mf@gmail.com>
2021-11-11 16:41:44 +0900

    revise

--- discordi/views.py
+++ discordi/views.py
@@ -18,7 +18,7 @@ class APISubmitView(View):
 
         atd = Attendances(
             login = str(body),
-            mtg_id = "0106bb5f-e842-49c0-86b9-b3882fdee26d",
+            mtg_id = "4639b69f-f84e-4cba-90a2-24c0c6bee98e",
             bw_id = "",
             ip_address = "",
             attend = True,
@@ -28,7 +28,7 @@ class APISubmitView(View):
         atd.save()
 
         return HttpResponse("Successful created new api doc!", status=201)
-    
+
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
